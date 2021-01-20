a0be086ff1be50d819d9d83edec6e4c76e55bc3e
Henrique Xavier <xavier.henrique@hotmail.com>
2021-01-19 21:11:52 -0300

    adicionado as imagens e preparando para o Vercel

--- src/Pages/Main/index.js
+++ src/Pages/Main/index.js
@@ -17,11 +17,6 @@ function Main(){
   </Container>
 
   <Container fluid className=" my-5">
-  <Image src="img/valorant.png" fluid/>
-  <h1>VALORANT</h1>
-  <span>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.</span>
-  </Container>
-  <Container fluid className=" my-5">
   <Image src="img/camps.png" fluid/>
   <h1>COMPETIÇÕES</h1>
   <span>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.</span>
